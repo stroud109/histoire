442987bd5884c80ff6e8419f8c17790e3d0c73e6
Stephanie Stroud <stroud109@gmail.com>
2014-04-24 09:58:49 -0700

    Designing database

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from sqlalchemy.types import Enum
 from sqlalchemy import (
     Column,
     create_engine,
+    ForeignKey,
     Integer,
     # String,
     Text,
@@ -25,24 +26,23 @@ Base = declarative_base()
 Base.query = session.query_property()
 
 
-# class User(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String(80), unique=True)
-#     email = Column(String(120), unique=True)
-
-#     TODO: figure out if I want a login feature
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(Text(), unique=True)
+    email = Column(Text(), unique=True)
 
 
-class Pins(Base):
+class Pin(Base):
     __tablename__ = 'pins'
     id = Column(Integer, primary_key=True)
     genre = Column(Enum('history', 'historical_fiction'))
-    name = Column(Text())
+    title = Column(Text())
     birth_year = Column(Text())
     death_year = Column(Text())
     bio = Column(Text())
     url = Column(Text())
+    pin_author = Column(Integer(), ForeignKey('users.id'), nullable=False)
 
     # TODO: init database
 
